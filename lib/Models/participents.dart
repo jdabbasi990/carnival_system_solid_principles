34dff6f1982e8c3831f2a33cf4832091d632e157
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2024-05-31 21:59:31 -0400

    Structure complete participent class.

--- lib/Models/participents.dart
+++ lib/Models/participents.dart
@@ -1,9 +1,29 @@
 class Participents {
   String _name;
-  int? age;
+  int _age;
 
-  Participents(this._name, this.age);
-
+  Participents(this._name, this._age);
+// getter for name property.
   String get name => _name;
-  set name(String name) => _name = name;
+
+  //setter for name property.
+  set name(String name) {
+    if (name.isNotEmpty) {
+      _name = name;
+    } else {
+      print('Empty Name can not be assigned.');
+    }
+  }
+
+  //getter for age property
+  int get age => _age;
+
+  //Setter for Age property.
+  set age(int age) {
+    if (age > 0) {
+      _age = age;
+    } else {
+      print('Age must be positive');
+    }
+  }
 }
