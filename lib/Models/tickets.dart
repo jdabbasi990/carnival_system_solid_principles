c6a9748ab1a9e73da5bf55265f20703635bc783c
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2024-05-31 22:23:09 -0400

    Ticket class strcture updated.

--- lib/Models/tickets.dart
+++ lib/Models/tickets.dart
@@ -1,1 +1,25 @@
-class Tickets {}
+import 'package:carnival_system_solid_principles/Models/participents.dart';
+
+class Tickets {
+  String _eventName;
+  Participents _participents;
+
+  Tickets(this._eventName, this._participents);
+//get event name
+  String get eventName => _eventName;
+//set eventname
+  set eventName(String eventName) {
+    if (eventName.isNotEmpty) {
+      _eventName = eventName;
+    } else {
+      print('Event can not be empty');
+    }
+  }
+
+//get partcipent
+  Participents get participents => _participents;
+//set particepent
+  set participents(Participents partcipent) {
+    _participents = partcipent;
+  }
+}
